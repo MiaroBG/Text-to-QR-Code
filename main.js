425c10702fbfe60cd87b51d39b97710bca186b4a
MiaroBG <miaronjato@gmail.com>
2025-09-13 21:31:26 +0300

    Add files via upload

---
+++ main.js
@@ -0,0 +1,11 @@
+function generateQrCode() {
+  var text = document.getElementById("text").value;
+  var qrcode = new QRCode(document.getElementById("qrcode"), {
+    text: text,
+    width: 128,
+    height: 128,
+    colorDark: "#000000",
+    colorLight: "#ffffff",
+    correctLevel: QRCode.CorrectLevel.H
+  })
+}
